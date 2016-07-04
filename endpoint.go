43459cce677c3e9052cca0a8e5a47be2e2b7a865
ldinc <drogunov.igor@gmail.com>
2016-07-04 16:50:05 +0300

    added: endpoints

---
+++ endpoint.go
@@ -0,0 +1,8 @@
+package mcloudru_ru
+
+const (
+	Domain = "mail.ru"
+	Cloud  = "https://cloud.mail.ru"
+	Auth   = "https://auth.mail.ru"
+	Public = Cloud + "/public/"
+)
--- test/testclient.go
+++ test/testclient.go
@@ -5,7 +5,16 @@ import (
 	"fmt"
 )
 
+func PrintEndpoints() {
+	fmt.Println("endpoints:")
+	fmt.Println("domain: " + mcloud.Domain)
+	fmt.Println("cloud: " + mcloud.Cloud)
+	fmt.Println("auth: " + mcloud.Auth)
+	fmt.Println("pub: " + mcloud.Public)
+}
+
 func main() {
 	fmt.Println("testing client ...")
+	PrintEndpoints()
 	mcloud.Hello()
 }
